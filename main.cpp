31cf9ef9fe6be27541c060d1274ecdf0d0e04d19
nferencko <nicholasferencko@gmail.com>
2024-07-11 01:35:40 +0000

    mg

--- main.cpp
+++ main.cpp
@@ -1,35 +1,34 @@
 
 
-#include<iostream>
+#include <iostream>
+#include <exception>
 
 using namespace std;
 
-void reverse (int &num1 , int &num2);
 
 int main(){
     
-    int num1, num2;
+string name;
+int number;
+size_t sz;
+bool bValidInput = false;
 
-    cout << "Enter the first number: ";
-    cin >> num1;
-    cout << "Enter the second number: ";
-    cin >> num2;
-
-    cout << "You entered " << num1 << " and " << num2 << endl;
-
-    reverse(num1,num2);
-    cout << "Numbers reverse " << num1 << " and " << num2;
-
-    
-    
-    return EXIT_SUCCESS;
+while (!bValidInput){
+    try{
+        cout << "enter a number ";
+    getline(cin, name);
+    number = stoi(name, &sz);
+    if(name.substr(sz).length() != 0){
+        throw exception();
+    }
+    cout << "You entered " << number << endl;
+    bValidInput = true;
+    }
+    catch(exception &e)
+    {
+        // Do nothing
+    }   
 }
 
-void reverse (int &num1 , int &num2){
-
-    int originalNum1 = num1;
-    num1 = num2;
-    num2 = originalNum1;
-
-    
+    return EXIT_SUCCESS;
 }
